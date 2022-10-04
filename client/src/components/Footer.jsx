6a57f6ef4c7e6119528dfbd66feaecd39524cd2a
juhyoungRyu <thinkoutside2004@gmail.com>
2022-10-05 01:53:08 +0900

    22.10.05 : Navigation + Footer Design

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import "../Compo.css";
+import { AiOutlineInstagram } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <div className="Footer">
-      <section></section>
+      <section className="footerTitle">
+        <h3>Korean Oriental And Asian</h3>
+      </section>
+      <section className="footerBold">
+        <section className="instaZone">
+          <AiOutlineInstagram className="instagram" />
+        </section>
+        <h4>T. 010-0000-0000</h4>
+      </section>
+      <section className="footerSlim">
+        COMPANY: 주식회사 코아 OWNER: 이정호 <br />
+        ADDRESS: 남양주시 와부읍 덕소로 000, 00
+        <br />
+        사업자번호: 000-00-00000 MAIL: jungholee@email.com
+      </section>
     </div>
   );
 };
