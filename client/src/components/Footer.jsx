1c36c80409af1026610ba83b37d0adcc13e091ad
juhyoungRyu <thinkoutside2004@gmail.com>
2022-10-05 01:59:16 +0900

    22.10.05 : overflow-x 문제 확인

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -15,10 +15,10 @@ const Footer = () => {
         <h4>T. 010-0000-0000</h4>
       </section>
       <section className="footerSlim">
-        COMPANY: 주식회사 코아 OWNER: 이정호 <br />
+        COMPANY: 주식회사 코아 | OWNER: 이정호 <br />
         ADDRESS: 남양주시 와부읍 덕소로 000, 00
         <br />
-        사업자번호: 000-00-00000 MAIL: jungholee@email.com
+        사업자번호: 000-00-00000 | MAIL: jungholee@email.com
       </section>
     </div>
   );
