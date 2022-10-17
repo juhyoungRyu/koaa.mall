8ce277c7d564ba81d3149574cc5d06719bd424b2
sungwookang <sungwookang11@gmail.com>
2022-10-17 16:37:26 +0900

    22. 10. 17 : shop 구현 및 폰트 적용

---
+++ client/src/components/Shop.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "../Compo.css";
+
+const Shop = () => {
+
+    return (
+        <div className="Shop">
+            <div className="shopNav">
+                <span className="S_all usher">ALL |</span>
+                <span className="S_outer usher"> OUTER  |</span>
+                <span className="S_top usher"> TOP | </span>
+                <span className="S_bottom usher"> BOTTOM | </span>
+                <span className="S_acc usher"> ACC</span>
+            </div>
+            <div className="S_productsCon">
+                <div className="S_product">
+                    <div className="S_pdtImg"></div>
+                    <span className="S_pdtName">[22FW] NO NEED EDUCATED HOOD</span>
+                    <span className="S_price">30,000 KRW</span>
+                </div>
+                <div className="S_product">
+                    <div className="S_pdtImg"></div>
+                    <span className="S_pdtName">[22FW] KOAA CANDY SWEATSHIRT</span>
+                    <span className="S_price">28,000 KRW</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
