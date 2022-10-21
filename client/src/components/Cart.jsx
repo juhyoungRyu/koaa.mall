e9f339d4a96a1c8e302ebb19242f1e59576021d4
sungwookang <sungwookang11@gmail.com>
2022-10-21 16:41:15 +0900

    22.10.21 : Cart 목록 작업중

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -4,7 +4,33 @@ import "../Compo.css";
 const Cart = () => {
 
     return(
-        <div className="Cart">cart</div>
+        <div className="Cart">
+            <section className="C_titlesec">
+                <h3 className="C_title">장바구니</h3>
+            </section>
+            <section className="C_itemCon">
+                <div className="C_item">
+                    <div className="C_itemImg">
+                        <div className="checkbox">
+                            <div className="core"></div>
+                        </div>
+                        <img src="" alt="" className="C_img" />
+                    </div>
+                    <div className="C_infos">
+                        <div clasesName="C_high">
+                            <span className="C_pdtname">[22FW] NO NEED EDUCATED HOOD</span>
+                            <div className="C_xBtn"></div>
+                        </div>
+                        <div className="C_mid">
+                            <span className="C_size">XL</span>
+                        </div>
+                        <div className="C_low">
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
     );
 };
 
